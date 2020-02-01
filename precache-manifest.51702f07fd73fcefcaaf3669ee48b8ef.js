af8c070084cd3c36d84b46524003cafe13378d50
muhammadkevinpf <muhammadkevinpf@gmail.com>
2020-02-02 00:15:29 +0700

    Updates

---
+++ precache-manifest.51702f07fd73fcefcaaf3669ee48b8ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cb85c222a9be78800f3ef16f4bfbd0",
+    "url": "muhammadkevinpf.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "d2e591db7ddd8451ea24",
+    "url": "muhammadkevinpf.github.io/portfolio/static/css/main.11d69052.chunk.css"
+  },
+  {
+    "revision": "95ee4c14d63beb51f604",
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/2.69898af7.chunk.js"
+  },
+  {
+    "revision": "65ef299342e8ac138107060d5debc227",
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/2.69898af7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2e591db7ddd8451ea24",
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/main.48c15e3f.chunk.js"
+  },
+  {
+    "revision": "1f33840c1e92186497ee",
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/runtime-main.1ca5a309.js"
+  }
+]);

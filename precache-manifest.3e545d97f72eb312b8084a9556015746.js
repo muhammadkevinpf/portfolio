f170cc49a617fba3099ab3304ebea25bd179159d
muhammadkevinpf <muhammadkevinpf@gmail.com>
2020-02-25 15:08:11 +0700

    Updates

--- precache-manifest.3e545d97f72eb312b8084a9556015746.js
+++ precache-manifest.3e545d97f72eb312b8084a9556015746.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d9596ae5510006a046fc6518e0c1b499",
-    "url": "./index.html"
+    "url": "muhammadkevinpf.github.io/portfolio/index.html"
   },
   {
     "revision": "219809cc159fc3d60185",
-    "url": "./static/css/main.439a4651.chunk.css"
+    "url": "muhammadkevinpf.github.io/portfolio/static/css/main.439a4651.chunk.css"
   },
   {
     "revision": "93b9d56b0ed2d810bd40",
-    "url": "./static/js/2.dc79fc8d.chunk.js"
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/2.dc79fc8d.chunk.js"
   },
   {
     "revision": "65ef299342e8ac138107060d5debc227",
-    "url": "./static/js/2.dc79fc8d.chunk.js.LICENSE"
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/2.dc79fc8d.chunk.js.LICENSE"
   },
   {
     "revision": "219809cc159fc3d60185",
-    "url": "./static/js/main.310474cb.chunk.js"
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/main.310474cb.chunk.js"
   },
   {
     "revision": "1f33840c1e92186497ee",
-    "url": "./static/js/runtime-main.1ca5a309.js"
+    "url": "muhammadkevinpf.github.io/portfolio/static/js/runtime-main.1ca5a309.js"
   }
 ]);
